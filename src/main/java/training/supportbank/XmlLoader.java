19564049204d07ad9b6ea99a3a4af41045be57d5
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-07-02 15:59:25 +0100

    Added logging to xml loader

--- src/main/java/training/supportbank/XmlLoader.java
+++ src/main/java/training/supportbank/XmlLoader.java
@@ -19,44 +19,71 @@ public class XmlLoader implements FileLoader {
     public List<Transaction> loadFile(String filename) throws Exception {
         LOGGER.info("Loading transactions from xml file " + filename);
         ArrayList<Transaction> transactions = new ArrayList<>();
+
+        File inputFile = new File(filename);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc;
         try {
-            File inputFile = new File(filename);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("SupportTransaction");
-            for (int i = 0; i < nList.getLength(); i++) {
-                Node nNode = nList.item(i);
-                if(nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    int numberOfDays = Integer.parseInt(eElement.getAttribute("Date"));
-                    LocalDate date = LocalDate.of(1900,1,1).plusDays(numberOfDays);
-                    String narrative = eElement
+            doc = dBuilder.parse(inputFile);
+        } catch (Exception e) {
+            LOGGER.error("Failed to parse xml file " + filename);
+            throw e;
+        }
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("SupportTransaction");
+        int errors = 0;
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+            if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                int numberOfDays = Integer.parseInt(eElement.getAttribute("Date"));
+                LocalDate date = LocalDate.of(1900,1,1).plusDays(numberOfDays);
+                String narrative = "";
+                try {
+                    narrative = eElement
                             .getElementsByTagName("Description")
                             .item(0)
                             .getTextContent();
+                } catch (Exception e) {
+                    LOGGER.error("Unable to extract narrative in entry " + i);
+                    errors++;
+                }
+                Currency amount = new Currency(0);
+                try {
                     String tempAmount = eElement
                             .getElementsByTagName("Value")
                             .item(0)
                             .getTextContent();
-                    Currency amount = new Currency(tempAmount);
+                    amount = new Currency(tempAmount);
+                } catch (Exception e) {
+                    LOGGER.error("Unable to extract amount in entry " + i);
+                    errors++;
+                }
+                String fromAccount = "", toAccount = "";
+                try {
                     NodeList parties = eElement.getElementsByTagName("Parties");
                     Element partiesElement = (Element) parties.item(0);
-                    String fromAccount = partiesElement
+                    fromAccount = partiesElement
                             .getElementsByTagName("From")
                             .item(0)
                             .getTextContent();
-                    String toAccount = partiesElement
+                    toAccount = partiesElement
                             .getElementsByTagName("To")
                             .item(0)
                             .getTextContent();
-                    transactions.add(new Transaction(amount, fromAccount, toAccount, narrative, date));
+                } catch (Exception e) {
+                    LOGGER.error("Unable to extract parties in entry " + i);
+                    errors++;
                 }
+                transactions.add(new Transaction(amount, fromAccount, toAccount, narrative, date));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        if(errors > 0) {
+            LOGGER.error("Unable to load " + filename + ", encountered " + errors + " errors");
+            throw new Exception();
+        }
+
         return transactions;
     }
 }
